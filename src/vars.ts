c18e7c7734a77777b2025087499f49e76ed49fc6
Josué lobo <darksiders20005@gmail.com>
2022-04-28 16:52:30 -0300

    feat: dot env bd implements

--- src/vars.ts
+++ src/vars.ts
@@ -1,11 +1,14 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const { env } = process
+const { env } = process;
 
 export default {
-  api: {
-    port: Number(env.PORT || 3001)
-  }
-}
+    api: {
+        port: Number(env.PORT || 3001)
+    },
+    db: {
+        url: env.DB_URL || 'mysql://root:root@127.0.0.1:3306/todos'
+    }
+};
